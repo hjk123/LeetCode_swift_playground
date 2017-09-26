fdb7553c969f470677eefc5ca28d51f3dac5876d
黄江昆 <huangjiangkun@ibona.cn>
2017-09-26 23:59:10 +0800

    finished house robber I,add question house robber II

--- LeetCode.playground/Pages/HouseRobber(198).xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/HouseRobber(198).xcplaygroundpage/Contents.swift
@@ -9,6 +9,25 @@
 
 class Solution {
     func rob(_ nums: [Int]) -> Int {
-        
+        guard nums.count > 0 else {
+            return 0
+        }
+        if nums.count == 1 {
+            return nums[0]
+        }
+        var result = nums[0]
+        var dp :[Int] = Array.init(repeating: 0, count: nums.count)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        if dp[1] > result {
+            result = dp[1]
+        }
+        for i in 2..<nums.count {
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+            if dp[i] > result {
+                result = dp[i]
+            }
+        }
+        return result
     }
 }
