74f060354e2a1307da8caffa50bada8da063f399
huangjiangkun <huangjiangkun@ibona.cn>
2017-10-14 15:10:43 +0800

    finish coins change

--- LeetCode.playground/Pages/CoinChange(322).xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/CoinChange(322).xcplaygroundpage/Contents.swift
@@ -16,12 +16,26 @@
 //Credits:
 //Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
 
-
+//这个问题就是0-1背包问题,用动态规划解决
+//状态的定义: f[i]: 表示钱数为i时找零的最少硬币个数
+//状态转移方程: f[i] = min(f[i-1],f[i-coins[j]] + 1)
 class Solution {
     func coinChange(_ coins: [Int], _ amount: Int) -> Int {
         guard coins.count > 0 else {
             return -1
         }
-        //
+        guard amount > 0 else {
+            return 0
+        }
+        var dp: [Int] = Array.init(repeating: amount + 1, count: amount + 1)
+        dp[0] = 0
+        for i in 1...amount {
+            for j in 0..<coins.count {
+                if coins[j] <= i {
+                    dp[i] = min(dp[i-1], dp[i-coins[j]] + 1)
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount]
     }
 }
