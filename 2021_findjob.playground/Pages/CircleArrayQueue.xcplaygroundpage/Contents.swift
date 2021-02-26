b779f1be44acb64f4369f65c4d33d6c7a1b078fd
黄江昆 <huangjk@dachentech.com.cn>
2021-02-26 09:45:04 +0800

    add problem

--- 2021_findjob.playground/Pages/CircleArrayQueue.xcplaygroundpage/Contents.swift
+++ 2021_findjob.playground/Pages/CircleArrayQueue.xcplaygroundpage/Contents.swift
@@ -48,3 +48,45 @@ class Consumer{
     
     
 }
+
+class ArrayQueue {
+    
+    var data: [Int]
+    
+    var head: Int = 0 , tail: Int = 0 , size: Int = 0
+    
+    init(size: Int) {
+        self.data = Array.init(repeating: 0, count: size)
+        self.size = size
+    }
+    
+    //入队 考虑tail 到队尾了 要数据迁移的情况（上面的环形数组 也能解决数据迁移的问题）
+    func enqueue(element: Int) -> Bool {
+        if tail == size {
+            //队列满了
+            if head == 0 {
+                return false
+            }
+            //数据迁移
+            for i in head..<tail {
+                data[i - head] = data[i]
+            }
+            head = 0
+            tail -= head
+        }
+        data[tail] = element
+        tail += 1
+        return true
+    }
+    
+    //出队
+    func dequeue(element: Int) -> Int? {
+        //队列空了
+        if head == tail {
+            return nil
+        }
+        let result = data[head]
+        head += 1
+        return result
+    }
+}
--- 2021_findjob.playground/Pages/CircleListQueue.xcplaygroundpage/Contents.swift
+++ 2021_findjob.playground/Pages/CircleListQueue.xcplaygroundpage/Contents.swift
@@ -2,6 +2,11 @@
 
 import Foundation
 
+//基于链表队列
+class ListQueue {
+    
+}
+
 //基于环形链表 队列
 class CircleListQueue {
     
